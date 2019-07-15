86e04e6be5f6b2f0c51364ef0993eb795d1fb4d1
zhongjincan <zhongjincan@banggood.com>
2019-07-15 09:53:59 +0800

    test

--- src/main/java/com/zjc/bs/BsApplication.java
+++ src/main/java/com/zjc/bs/BsApplication.java
@@ -9,7 +9,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.zjc.bs.mapper") //扫描的mapper
 @SpringBootApplication
 public class BsApplication {
-
+//asssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
     public static void main(String[] args) {
         SpringApplication.run(BsApplication.class, args);
     }
